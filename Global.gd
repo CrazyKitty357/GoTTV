5a6cabc711c9ae0161271f8d19a94a0e074f741e
CrazyKitty357 <73915693+CrazyKitty357@users.noreply.github.com>
2023-10-08 23:53:08 -0400

    Update Global.gd

--- Global.gd
+++ Global.gd
@@ -4,5 +4,5 @@ var streamer = ""
 var badges = true
 var mod = false
 var watermark = " [i](this was sent from godot)[/i]"
-var client_id = "56vyt9okhhepe0ouqk72tb2pehcshr"
-var client_secret = "rgkr5eu4abldfakun78qercv5m019m"
+var client_id = "client_id"
+var client_secret = "client_secret"
